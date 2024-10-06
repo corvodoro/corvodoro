050eedee484949f51ebd7ee54542d33379705bd7
malevitzch <malevitzch@gmail.com>
2024-10-06 11:47:48 +0200

    Added tests for scrolling

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,16 @@ export default function Home() {
     <main>
       <article id="about">
         <h1 className="text-5xl w-full text-center font-bold">About Us</h1>
+        <p>We are a small team from Poland, working on web3 projects"</p>
+        <img src="corvodoro_logo.png"></img>
       </article>
       <article id="projects">
         <h1 className="text-5xl w-full text-center font-bold">Projects</h1>
-        
+        <img src="corvodoro_logo.png"></img>
       </article>
       <article id="team">
         <h1 className="text-5xl w-full text-center font-bold">Team</h1>
+        <img src="corvodoro_logo.png"></img>
       </article>
     </main>
   );
