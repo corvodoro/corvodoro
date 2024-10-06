1ebe00c9889665953c00462a2838a78bd353000d
malevitzch <malevitzch@gmail.com>
2024-10-06 11:51:05 +0200

    Added a margin for main so that the sidebar does not overlap with text when not expanded

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className = "ml-20">
       <article id="about">
         <h1 className="text-5xl w-full text-center font-bold">About Us</h1>
-        <p>We are a small team from Poland, working on web3 projects"</p>
+        <p>We are a small team from Poland, working on web3 projects</p>
         <img src="corvodoro_logo.png"></img>
       </article>
       <article id="projects">
